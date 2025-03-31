113aca84e6a143607a29c09172538eac7a592536
RND-HW3 <RND-HW3@RND-HW3>
2025-03-31 10:39:18 +0900

    초기 커밋.

---
+++ AutoImplementedProperty/MainApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AutoImplementedProperty
+{
+    class BirthdayInfo
+    {
+        public DateTime Birthday { get; set; } = new DateTime(1, 1, 1);
+        public string Name { get; set; } = "Unknown";        
+        public int Age
+        {
+            get
+            {
+                return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
+            }
+        }
+    }                
+    
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            BirthdayInfo birth = new BirthdayInfo();
+            Console.WriteLine($"Name: {birth.Name}");
+            Console.WriteLine($"Birthday: {birth.Birthday}");
+            Console.WriteLine($"Age: {birth.Age}");
+
+            birth.Name = "dawon";
+            birth.Birthday = new DateTime(2000, 12, 16);
+
+            Console.WriteLine($"\nName: {birth.Name}");
+            Console.WriteLine($"Birthday: {birth.Birthday.ToShortDateString()}");
+            Console.WriteLine($"Age: {birth.Age}");
+        }
+    }
+}
